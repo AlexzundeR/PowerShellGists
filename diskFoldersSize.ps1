b6e0f12751bf65adc693fe52d86ff7d947b32e27
AlexzundeR <grandflash@mail.ru>
2016-01-25 12:17:37 +0300

    add folder size inspector script

---
+++ diskFoldersSize.ps1
@@ -0,0 +1,4 @@
+﻿. 'D:\Job\powershell\get-FolderSize.ps1'
+
+
+Get-ChildItem 'C:\Program Files (x86)' -Force | ? {$_.GetType() -eq [System.IO.DirectoryInfo]} | % {$_.FullName} | Get-FolderSize -Unit 'GB' | ? {$_.FolderSize -gt 1}
